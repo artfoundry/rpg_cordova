08ac17944871f3dea334c41f7c917d35dfafeb8f
David Markowitz <david_markowitz@apple.com>
2017-01-19 17:46:30 -0800

    Styling for status bar and modal.

--- www/js/ui-class.js
+++ www/js/ui-class.js
@@ -8,17 +8,23 @@ class UI {
             "dialogHeader"  : "Welcome to Monsters!",
             "gameIntro"     : "You have entered a dark crypt in search of a valuable artifact, but little did you know that ancient evil and chaotic denizens wander here.",
             "instructions"  : "You must kill all the creatures to survive and leave with your life! If you are attacked more than three times, you will die.  Good luck!",
-            "tips"          : "There are two types of monsters: a Queen, and her minions.  The Queen spawns a minion every turn and must be attacked three times to kill her.  While she cannot attack, her minions can.  Each minion will attack if you approach within one square of it, but can be killed with one hit."
-        }
+            "tips"          : "There are two types of monsters: a Queen, and her minions.  The Queen spawns a minion every turn and must be attacked three times to kill her.  While she cannot attack, her minions can.  Each minion will attack if you approach within one square of it, but can be killed with one hit.",
+            "gameOverDead"  : "The hideous monster sucks the life out of you.  You are dead.",
+            "gameOverWin"   : "You've finally slaughtered every last creature and make it out alive!"
+        };
+        this.kills = 0;
+        $("#pc-health span").on()
 
         //temp
         let self = this;
         $("#open-modal").click(function() {
+            self.kills += 1;
             self.modalOpen([
                 {"element" : ".dialog-body", "text" : "gameIntro"},
                 {"element" : "#dialog-header", "text" : "dialogHeader"}], [
-                {"id" : "#button-ok", "action" : self.modalClose},
-                {"id" : "#button-start", "action" : self.updateValue, "params" : {"id" : "#pc-health", "value" : 1}} ]
+                {"id" : "#button-start", "action" : self.modalClose},
+                {"id" : "#button-restart", "action" : self.updateValue, "params" : {"id" : "#pc-health .status-value", "value" : 1}},
+                {"id" : "#button-tips", "action" : self.updateValue, "params" : {"id" : "#pc-health .status-value", "value" : -1}}]
             );
         });
     }
@@ -50,6 +56,14 @@ class UI {
     }
 
     updateValue(params) {
-        $(params.id + " span").text(params.value);
+        let $element = $(params.id),
+            $bubble = $(params.id + " .life-bubble:last"),
+            newBubble = "<span class='life-bubble'></span>";
+
+        if (params.id.includes("#pc-health")) {
+            params.value < 0 ? $bubble.remove() : $element.append(newBubble);
+        }
+        else
+            $element.text(params.value);
     }
 }
