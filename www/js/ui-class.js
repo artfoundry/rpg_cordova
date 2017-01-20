46fa708580e32912ba66f80ec1f9071701be4731
David Markowitz <david_markowitz@apple.com>
2017-01-19 16:03:22 -0800

    Adding modal and status value updating functionality and further styling.

--- www/js/ui-class.js
+++ www/js/ui-class.js
@@ -6,10 +6,21 @@ class UI {
     constructor() {
         this.dialogs = {
             "dialogHeader"  : "Welcome to Monsters!",
-            "gameIntro"     : "You have entered a dark crypt in search of a valuable artifact, but little did you know that ancient evil and chaos denizens wander here.",
+            "gameIntro"     : "You have entered a dark crypt in search of a valuable artifact, but little did you know that ancient evil and chaotic denizens wander here.",
             "instructions"  : "You must kill all the creatures to survive and leave with your life! If you are attacked more than three times, you will die.  Good luck!",
             "tips"          : "There are two types of monsters: a Queen, and her minions.  The Queen spawns a minion every turn and must be attacked three times to kill her.  While she cannot attack, her minions can.  Each minion will attack if you approach within one square of it, but can be killed with one hit."
         }
+
+        //temp
+        let self = this;
+        $("#open-modal").click(function() {
+            self.modalOpen([
+                {"element" : ".dialog-body", "text" : "gameIntro"},
+                {"element" : "#dialog-header", "text" : "dialogHeader"}], [
+                {"id" : "#button-ok", "action" : self.modalClose},
+                {"id" : "#button-start", "action" : self.updateValue, "params" : {"id" : "#pc-health", "value" : 1}} ]
+            );
+        });
     }
 
     /**
@@ -18,19 +29,27 @@ class UI {
      * @param messages: Object with a list of element IDs (referring to the dialog elements) referencing keys of the UI's dialogs Object
      * @param buttons: Array of button IDs to show
      */
-    dialogOpen(messages, buttons) {
-        $(".dialog").toggle();
-        for (let message in messages) {
-            if (Object.prototype.hasOwnProperty.call(messages, message)) {
-                $(".dialog" > message.element).innerText(message.text);
-            }
+    modalOpen(messages, buttons) {
+        $(".modal").show();
+        for (let i = 0; i < messages.length; i++) {
+            $(".modal " + messages[i].element).text(this.dialogs[messages[i].text]);
         }
         for (let i = 0; i < buttons.length; i++) {
-            $(".dialog" > buttons[i]).toggle();
+            $(".modal " + buttons[i].id).show().click(function() {
+                if (buttons[i].params)
+                    buttons[i].action(buttons[i].params);
+                else
+                    buttons[i].action();
+            });
         }
     }
 
-    updateValue(id, value) {
-        $("#" + id).innerText(value);
+    modalClose() {
+        $(".modal").hide();
+        $(".modal button").hide();
+    }
+
+    updateValue(params) {
+        $(params.id + " span").text(params.value);
     }
 }
